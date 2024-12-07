c6e7c3d693b35d972d61d21c636b4372ce900b17
guwls <guswls2654@naver.com>
2024-12-07 17:11:38 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/hyunjin/portfolio/domain/entity/BaseEntity.kt
+++ src/main/kotlin/com/hyunjin/portfolio/domain/entity/BaseEntity.kt
@@ -1,4 +1,21 @@
 package com.hyunjin.portfolio.domain.entity
 
+import jakarta.persistence.Column
+import org.springframework.cglib.core.Local
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
+
 abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    var createdDateTime: LocalDateTime = LocalDateTime.now()
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    var updatedDateTime: LocalDateTime = LocalDateTime.now()
+
+
 }
